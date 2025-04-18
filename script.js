627e62bc68ac2491d89b74725d0c7c1f63cad295
ANANDITA NAGPAL <143868143+AnanditaNagpal@users.noreply.github.com>
2025-04-18 16:41:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("darkModeToggle");
-  toggle.addEventListener("click", () => {
+function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
-  });
-});
+}
